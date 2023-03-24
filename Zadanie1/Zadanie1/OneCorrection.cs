6b9b735e84a6b2a7a36153d8ce14812db86e2a9c
Kacper Czernik <Kacper-czernik@wp.pl>
2023-03-24 20:19:48 +0100

    Zadanie1 | encodina single fucking word works

--- Zadanie1/Zadanie1/OneCorrection.cs
+++ Zadanie1/Zadanie1/OneCorrection.cs
@@ -50,5 +50,34 @@ namespace Zadanie1
         new int[] {0,0,1,0,1,0,1,1,1,1,0,1},
         new int[] {0,0,0,1,1,1,1,0,1,1,1,1}
                                            };
+
+
+        public void encodeWord(int[] word)
+        {
+            int position=0;
+            int[] output = new int[12];
+            for (int i=0; i<12; i++) 
+            {output[i] = 0;}
+
+            //position  = position of this bit in the word
+            //output    = holds an output of a function
+
+            for (int i=0; i!=8;i++) 
+            {
+                for (int j=0; j!=12; j++) 
+                {
+                    output[j] = output[j] + word[i] * G[i][j];
+                }
+            }
+
+            for (int i=0; i!=12; i++)
+            {
+                output[i] = output[i]%2;
+            }
+        }
+
     }
+
+   
+
 }
